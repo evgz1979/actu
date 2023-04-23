bc3dc3316cec2e6aa0dbf4c2be1ad0d9b42bcfed
evgz1979 <i@evgz.ru>
2023-04-23 12:29:52 +0300

    init

---
+++ main.py
@@ -0,0 +1,56 @@
+import datetime
+
+from settings import *
+from orm import *
+import pandas as pd
+import finplot as fplt
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
+
+
+def plot1(df1):  # temp function
+    fplt.candlestick_ochl(df1[['open', 'close', 'high', 'low']])
+    fplt.show()
+
+
+def candles_to_df(candles):  # temp function
+    df = pd.DataFrame(candles, columns='ts open high low close volume'.split()).astype(
+        {'ts': int, 'open': float, 'high': float, 'low': float, 'close': float, 'volume': float})
+    df['dt'] = df.apply(lambda x: timestamp2iso(x['ts'], format='%Y-%m-%d %H:%M:%S'), axis=1)
+    return df.set_index('ts')
+
+
+def candles_to_df2(candles):  # temp function
+    df = pd.DataFrame(candles, columns='dt open high low close volume'.split()).astype(
+        {'dt': str, 'open': float, 'high': float, 'low': float, 'close': float, 'volume': float})
+    df['dt'] = df.apply(lambda x: timestamp2iso(x['dt'], format='%Y-%m-%d %H:%M:%S'), axis=1)
+    temp_interval_as_int = 1  # позже нужна таблица интервалов и их названий
+    df.insert(1, 'interval', 1)
+    return df.set_index('dt')
+
+
+def get_candles(symbol_name, interval):
+    a = kus.fetch_ohlcv(symbol_name, interval, limit=500)
+    return a
+
+
+def log(a):
+    print(a)
+
+
+log('started')
+engine = create_engine("sqlite:////Users/evgz/py/b3/db/01.db", echo=False)
+engine.connect()
+Base.metadata.create_all(engine)
+session = Session(engine)
+
+# df = candles_to_df(get_candles(symbol_name_ku, '1w'))  # -- так работает с plot()
+# plot1(df)
+
+df = candles_to_df2(get_candles(symbol_name_ku, '1w'))  # -- так работает с базой
+print(df)
+print('max=', df['high'].max(), '; min=', df['low'].min())
+
+df.to_sql('candles', con=engine, if_exists='append')
+# потом удалить неуникальные значения надо
+# -- унифицировать df для базы и графика
