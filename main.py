62e280289a48324cfc7cb3e49d2bfc3d43837b46
evgz1979 <i@evgz.ru>
2023-06-01 20:12:20 +0300

    Migrate to SQLModel

--- main.py
+++ main.py
@@ -1,48 +1,12 @@
-import datetime
-
-from settings import *
 from orm import *
-import pandas as pd
-import finplot as fplt
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session, sessionmaker
+from data import *
+import finplot as fp
 
+if __name__ == '__main__':
+    df = candles_to_df3(get_candles(symbol_name, '1w'))  # -- так работает с базой
+    print(df)
+    print('max=', df['high'].max(), '; min=', df['low'].min())
+    fp.candlestick_ochl(df[['open', 'close', 'high', 'low']])
+    fp.show()  # унифицировать df для базы и графика
+    df.to_sql('candles', con=engine, if_exists='append')  # потом удалить неуник значения
 
-def plot1(df1):  # temp function
-    fplt.candlestick_ochl(df1[['open', 'close', 'high', 'low']])
-    fplt.show()
-
-
-def candles_to_df3(candles):  # temp function
-    df = pd.DataFrame(candles, columns='ts open high low close volume'.split()).astype(
-        {'ts': int, 'open': float, 'high': float, 'low': float, 'close': float, 'volume': float})
-    df.insert(1, 'dt', '')
-    df['dt'] = df.apply(lambda x: timestamp2iso(x['ts'], format='%Y-%m-%d %H:%M:%S'), axis=1)
-    temp_interval_as_int = 1  # позже нужна таблица интервалов и их названий
-    df.insert(1, 'interval', 1)
-    return df.set_index('ts')
-
-
-def get_candles(symbol_name, interval):
-    a = kus.fetch_ohlcv(symbol_name, interval, limit=500)
-    return a
-
-
-def log(a):
-    print(a)
-
-
-log('started')
-engine = create_engine("sqlite:////Users/evgz/py/b3/db/01.db", echo=False)
-engine.connect()
-Base.metadata.create_all(engine)
-session = Session(engine)
-
-df = candles_to_df3(get_candles(symbol_name_ku, '1w'))  # -- так работает с базой
-# print(df)
-# print('max=', df['high'].max(), '; min=', df['low'].min())
-plot1(df)
-
-df.to_sql('candles', con=engine, if_exists='append')
-# потом удалить неуникальные значения надо
-# -- унифицировать df для базы и графика
---
+++ orm01.py
@@ -0,0 +1,114 @@
+from sqlalchemy import create_engine, MetaData, Table, Integer, Float, String, \
+    Column, DateTime, ForeignKey, Numeric, SmallInteger
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
+
+from datetime import datetime
+from typing import Optional
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+
+Base = declarative_base()
+
+
+# все инициализации базы, проверка на таблицы, если что создаю, если базы нет - создаю и служебные таблицы по умолчанию из констант создаю
+# проверка на полноту данных (например по свечам для котируемых инструментов) и на дубли и корректность (на валидность короче) и др
+class DbEngine:
+    pass
+
+
+class Interval(Base):
+    __tablename__ = 'intervals'
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(50), nullable=False)
+    comment = Column(String(100), nullable=False)
+
+
+class Candle(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    interval: int
+    ts: int
+    dt: datetime
+    open: float
+    high: float
+    low: float
+    close: float
+    volume: float
+
+
+class Candle(Base):
+    __tablename__ = 'candles'
+    id = Column(Integer(), primary_key=True)
+    interval = Column(Integer(), nullable=False)
+    ts = Column(Integer(), nullable=False)
+    dt = Column(DateTime(), default=datetime.now)
+    open = Column(Float(), nullable=False)
+    high = Column(Float(), nullable=False)
+    low = Column(Float(), nullable=False)
+    close = Column(Float(), nullable=False)
+    volume = Column(Float(), nullable=False)
+
+
+class Symbol(Base):
+    __tablename__ = 'symbols'
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(100), nullable=False)
+    type = Column(String(100), nullable=False, comment='тип: спот, фьючерс или др')
+    exchange_name = Column(String(50), nullable=False, comment='имя биржи, на которой котируется символ')
+    dt_begin = Column(DateTime(), default=datetime.now)
+    dt_begin_historical = Column(DateTime(), default=datetime.now)
+
+
+class QuotedSymbol(Base):
+    __tablename__ = 'quotedsymbols'
+    id = Column(Integer(), primary_key=True)
+#    symbol_id --- связи::::
+
+
+class Customer(Base):
+    __tablename__ = 'customers'
+    id = Column(Integer(), primary_key=True)
+    first_name = Column(String(100), nullable=False)
+    last_name = Column(String(100), nullable=False)
+    username = Column(String(50), nullable=False)
+    email = Column(String(200), nullable=False)
+    created_on = Column(DateTime(), default=datetime.now)
+    updated_on = Column(DateTime(), default=datetime.now, onupdate=datetime.now)
+    orders = relationship("Order", backref='customer')
+
+
+class Item(Base):
+    __tablename__ = 'items'
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(200), nullable=False)
+    cost_price = Column(Numeric(10, 2), nullable=False)
+    selling_price = Column(Numeric(10, 2), nullable=False)
+    quantity = Column(Integer())
+
+
+class Order(Base):
+    __tablename__ = 'orders'
+    id = Column(Integer(), primary_key=True)
+    customer_id = Column(Integer(), ForeignKey('customers.id'))
+    date_placed = Column(DateTime(), default=datetime.now)
+    line_items = relationship("OrderLine", backref='order')
+
+
+class OrderLine(Base):
+    __tablename__ = 'order_lines'
+    id = Column(Integer(), primary_key=True)
+    order_id = Column(Integer(), ForeignKey('orders.id'))
+    item_id = Column(Integer(), ForeignKey('items.id'))
+    quantity = Column(SmallInteger())
+    item = relationship("Item")
+
+
+if __name__ == '__main__':
+    engine = create_engine("sqlite:////Users/evgz/py/b3/db/01.db", echo=True)
+    SQLModel.metadata.create_all(engine)
+    # engine.connect()
+    # Base.metadata.create_all(engine)
+    # session = Session(engine)
