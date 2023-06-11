28e9c299503d455d3993e2eac7954a3ecdb0b30b
evgz1979 <i@evgz.ru>
2023-06-11 19:51:46 +0300

    finplot + scv worked !

--- main.py
+++ main.py
@@ -4,33 +4,45 @@ import pandas as pd
 import mplfinance as mpf
 import finplot as fp
 
-
 print('>> actualiZator started')
 
 # get_candles(quoted_symbols[0].name, 'D')
-df = candles_to_df3(get_candles_old(symbol_name_ku, '1d'))
+# df = candles_to_df3(get_candles_old(symbol_name_ku, '1d'))
 # df.to_sql('candle', con=engine, if_exists='append')
 # print(df)
 
 # print('max=', df['high'].max(), '; min=', df['low'].min())
-fp.candlestick_ochl(df[['open', 'close', 'high', 'low']])
-fp.show()
+# fp.candlestick_ochl(df[['open', 'close', 'high', 'low']])
+# fp.show()
 
+
+# --------- finplot ---
 df = pd.read_csv('db/rts01.csv',
                  dtype={'date': str, 'time': str,
                         'open': float, 'high': float, 'low': float, 'close': float,
                         'volume': float})
-df['dt'] = pd.to_datetime(df['date'] + df['time'])
-# df.insert(0, 'ts', 0)
+df['dt'] = pd.to_datetime(df['date'] + df['time'])  # as DateTime
 df.drop('date', axis=1, inplace=True)
 df.drop('time', axis=1, inplace=True)
-# df['ts'] = df.apply(lambda x: iso2timestamp(x['dt'], format='%Y%m%d %H%M%S'), axis=1)
-# df.drop('dt', axis=1, inplace=True)
-# df.set_index('dt')
-df.index.name = 'dt'
-df.index = pd.DatetimeIndex(df['dt'])
-
+df['dt'] = df['dt'].astype(str)  # as str
+df.insert(0, 'ts', 0)
+df['ts'] = df.apply(lambda x: iso2timestamp(x['dt'], format='%Y-%m-%d %H:%M:%S'), axis=1)
+df.drop('dt', axis=1, inplace=True)
+df['ts'] = df['ts'].astype(int)
+# df.set_index('ts')
+df = df.set_index('ts')
 print(df)
+fp.candlestick_ochl(df[['open', 'close', 'high', 'low']])
+fp.show()
 
-mpf.plot(df, type='candle', warn_too_much_data=1000)
 
+# --------- mplfinance ---
+# df = pd.read_csv('db/rts01.csv',
+#                 dtype={'date': str, 'time': str,
+#                        'open': float, 'high': float, 'low': float, 'close': float,
+#                        'volume': float})
+# df['dt'] = pd.to_datetime(df['date'] + df['time'])
+# df.drop('date', axis=1, inplace=True)
+# df.drop('time', axis=1, inplace=True)
+# df.index = pd.DatetimeIndex(df['dt'])
+# mpf.plot(df, type='candle', warn_too_much_data=1000)
